da0a1196703260a4c29473bc0c51b35ee0585caf
honey khatri <honeykhatri110206@gmail.com>
2026-07-24 10:24:51 +0530

    Create calculator.java

---
+++ calculator.java
@@ -0,0 +1,32 @@
+class Calculator{
+  public dtatic void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter no1:");
+    int a=sc.nextInt();
+    System.out.println("enter no2:");
+    char b=sc.next();
+    System.out.println("Enter op:");
+    switch(op){
+      case '+':
+        System.out.println("Add :"+(a+b));
+        break;
+      case '-':
+        System.out.println("sub:"+(a-b));
+        break;  
+      case '*':
+        System.out.println("mul:"+(a*b));
+        break;  
+      case '/':
+        if(b==0){
+          System.out.println("Div by zero");
+        }
+        else{
+         System.out.println("div:"+(a/b));
+        }
+        break; 
+      default:
+        System.out.println("Invalid op");
+    }
+  }
+
+}
