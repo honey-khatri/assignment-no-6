9cf1fb0b9a98a3baf6a73cd21f65102a90edcc3b
honey khatri <honeykhatri110206@gmail.com>
2026-07-24 10:35:08 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 class Calculator{
-  public dtatic void main(String[] args){
+  public static void main(String[] args){
     Scanner sc=new Scanner(System.in);
     System.out.println("Enter no1:");
     int a=sc.nextInt();
