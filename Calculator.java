f9d2ffa6410776bca89d4b81f654b69446d6fbd1
honey khatri <honeykhatri110206@gmail.com>
2026-07-24 10:48:02 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,36 +1,43 @@
 import java.util.Scanner;
 
-class Calculator{
-  public static void main(String[] args){
-    Scanner sc=new Scanner(System.in);
-    System.out.println("Enter no1:");
-    int a=sc.nextInt();
-    System.out.println("enter no2:");
-    int b=sc.nextInt();
-    System.out.println("Enter op:");
-    char op = sc.next().charAt(0);
-    switch(op){
-      case '+':
-        System.out.println("Add :"+(a+b));
-        break;
-      case '-':
-        System.out.println("sub:"+(a-b));
-        break;  
-      case '*':
-        System.out.println("mul:"+(a*b));
-        break;  
-      case '/':
-        if(b==0){
-          System.out.println("Div by zero");
+class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter no1: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter no2: ");
+        int b = sc.nextInt();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        char op = sc.next().charAt(0);
+
+        switch (op) {
+            case '+':
+                System.out.println("Addition = " + (a + b));
+                break;
+
+            case '-':
+                System.out.println("Subtraction = " + (a - b));
+                break;
+
+            case '*':
+                System.out.println("Multiplication = " + (a * b));
+                break;
+
+            case '/':
+                if (b == 0) {
+                    System.out.println("Division by zero is not allowed.");
+                } else {
+                    System.out.println("Division = " + (a / b));
+                }
+                break;
+
+            default:
+                System.out.println("Invalid operator.");
         }
-        else{
-         System.out.println("div:"+(a/b));
-        }
-        break; 
-      default:
-        System.out.println("Invalid op");
+
+        sc.close();
     }
-  }
-
 }
-
